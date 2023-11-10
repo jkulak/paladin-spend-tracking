b9a431bf0679eea1d414da64fe3bbaed8a65136d
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-10 15:21:22 +0100

    aider: Implemented sorting by clicking on the column headers in the `DataTable` component.
    # Aider chat conversation:
    
    USER: when clicking the column header i want to be able to sort the table by values from that column. do not sort in me
    mory, rather request that data from the api, that is delivered with postgrest
    
    ASSISTANT: To implement sorting by clicking on the column headers in the `DataTable` component, we need to modify the `DataTable.js` file to handle the click events on the headers and update the API request accordingly. Here's how we can do it:
    
    1. Add click event handlers to each table header that will set the `sortColumn` and toggle the `sortDirection`.
    2. Update the `useEffect` hook to include `sortColumn` and `sortDirection` in its dependency array, so it re-fetches data when these values change.
    3. Modify the API request URL to include the sorting parameters.
    
    Let's start with the changes:
    
    ```javascript
    www/app/src/DataTable.js
    <<<<<<< SEARCH
        useEffect(() => {
            const sortQuery = sortColumn ? `order=${sortColumn}.${sortDirection}` : '';
            fetch(`http://localhost:3000/transaction_view?${sortQuery}`)
                .then(response => response.json())
                .then(data => setTransactions(data))
                .catch(error => console.error('Error fetching data: ', error));
        }, []);
    =======
        const handleHeaderClick = (column) => {
            setSortDirection(sortColumn === column && sortDirection === 'asc' ? 'desc' : 'asc');
            setSortColumn(column);
        };
    
        useEffect(() => {
            const sortQuery = sortColumn ? `order=${sortColumn}.${sortDirection}` : '';
            fetch(`http://localhost:3000/transaction_view?${sortQuery}`)
                .then(response => response.json())
                .then(data => setTransactions(data))
                .catch(error => console.error('Error fetching data: ', error));
        }, [sortColumn, sortDirection]);
    
        return (
            <div>
                <h2>Data Table</h2>
                <table>
                    <thead>
                        <tr>
                            <th onClick={() => handleHeaderClick('id')}>ID</th>
                            <th onClick={() => handleHeaderClick('date')}>Date</th>
                            <th onClick={() => handleHeaderClick('value')}>Value</th>
                            <th onClick={() => handleHeaderClick('payee_name')}>Payee Name</th>
                            <th onClick={() => handleHeaderClick('category_name')}>Category Name</th>
                            <th onClick={() => handleHeaderClick('note')}>Note</th>
                            <th onClick={() => handleHeaderClick('cleared')}>Cleared</th>
                        </tr>
                    </thead>
                    <tbody>
                        {transactions.map(transaction => (
                            <tr key={transaction.id}>
                                <td>{transaction.id}</td>
                                <td>{transaction.date}</td>
                                <td>{transaction.value}</td>
                                <td>{transaction.payee_name}</td>
                                <td>{transaction.category_name}</td>
                                <td>{transaction.note}</td>
                                <td>{transaction.cleared ? 'Yes' : 'No'}</td>
                            </tr>
                        ))}
                    </tbody>
                </table>
            </div>
        );
    >>>>>>> REPLACE
    ```
    
    This code adds a `handleHeaderClick` function that updates the sorting state when a header is clicked. It also updates the `useEffect` hook to re-fetch data when the sorting state changes and adds `onClick` handlers to each table header to trigger sorting. The API request URL is constructed with the sorting parameters to ensure the data is sorted server-side by PostgREST.

--- www/app/src/DataTable.js
+++ www/app/src/DataTable.js
@@ -5,13 +5,50 @@ const DataTable = () => {
     const [sortColumn, setSortColumn] = useState(null);
     const [sortDirection, setSortDirection] = useState('asc');
 
+    const handleHeaderClick = (column) => {
+        setSortDirection(sortColumn === column && sortDirection === 'asc' ? 'desc' : 'asc');
+        setSortColumn(column);
+    };
+
     useEffect(() => {
         const sortQuery = sortColumn ? `order=${sortColumn}.${sortDirection}` : '';
         fetch(`http://localhost:3000/transaction_view?${sortQuery}`)
             .then(response => response.json())
             .then(data => setTransactions(data))
             .catch(error => console.error('Error fetching data: ', error));
-    }, []);
+    }, [sortColumn, sortDirection]);
+
+    return (
+        <div>
+            <h2>Data Table</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th onClick={() => handleHeaderClick('id')}>ID</th>
+                        <th onClick={() => handleHeaderClick('date')}>Date</th>
+                        <th onClick={() => handleHeaderClick('value')}>Value</th>
+                        <th onClick={() => handleHeaderClick('payee_name')}>Payee Name</th>
+                        <th onClick={() => handleHeaderClick('category_name')}>Category Name</th>
+                        <th onClick={() => handleHeaderClick('note')}>Note</th>
+                        <th onClick={() => handleHeaderClick('cleared')}>Cleared</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.id}</td>
+                            <td>{transaction.date}</td>
+                            <td>{transaction.value}</td>
+                            <td>{transaction.payee_name}</td>
+                            <td>{transaction.category_name}</td>
+                            <td>{transaction.note}</td>
+                            <td>{transaction.cleared ? 'Yes' : 'No'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 
     return (
         <div>
