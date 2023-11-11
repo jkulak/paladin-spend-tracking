f9a798b9dc62e33f1c0724cb2a28fad88c10a466
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-11 17:04:44 +0100

    aider: Add test to check if after clicking the column, sorting works properly for ascending and descending.
    # Aider chat conversation:
    
    USER: add test to check if after clicking the column, sorting works properly for ascending and descending
    
    ASSISTANT: To add a test for checking the sorting functionality, we need to modify the `DataTable.test.js` file. We will use the `fireEvent` function from `@testing-library/react` to simulate a click event on the column headers.
    
    Here is the *SEARCH/REPLACE block* for the change:
    
    ```javascript
    www/app/src/DataTable.test.js
    <<<<<<< SEARCH
    import React from 'react';
    import { render } from '@testing-library/react';
    import '@testing-library/jest-dom';
    import { useDataLoader } from './DataLoader';
    import DataTable from './DataTable';
    
    jest.mock('./DataLoader');
    
    describe('DataTable', () => {
        it('renders the correct data when transactions state changes', () => {
    =======
    import React from 'react';
    import { render, fireEvent } from '@testing-library/react';
    import '@testing-library/jest-dom';
    import { useDataLoader } from './DataLoader';
    import DataTable from './DataTable';
    
    jest.mock('./DataLoader');
    
    describe('DataTable', () => {
        it('renders the correct data when transactions state changes', () => {
    >>>>>>> REPLACE
    ```
    
    ```javascript
    www/app/src/DataTable.test.js
    <<<<<<< SEARCH
            expect(getByText('Test note')).toBeInTheDocument();
        });
    });
    =======
            expect(getByText('Test note')).toBeInTheDocument();
        });
    
        it('sorts data correctly when column header is clicked', () => {
            useDataLoader.mockReturnValue({
                transactions: [
                    { id: 1, date: '2021-01-01', value: 100, payee_name: 'John Doe', category_name: 'Groceries', note: 'Test note' },
                    { id: 2, date: '2021-02-01', value: 200, payee_name: 'Jane Doe', category_name: 'Rent', note: 'Rent payment' },
                    { id: 3, date: '2021-03-01', value: 300, payee_name: 'Bob Smith', category_name: 'Utilities', note: 'Electricity bill' },
                    { id: 4, date: '2021-04-01', value: 400, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
                    { id: 5, date: '2021-05-01', value: 500, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
                ],
                error: null,
            });
    
            const { getByText } = render(<DataTable searchTerm="" />);
    
            // Click the 'Value' column header to sort by value in ascending order
            fireEvent.click(getByText('Value'));
            // Check that the first row has the smallest value
            expect(getByText('100')).toBeInTheDocument();
    
            // Click the 'Value' column header again to sort by value in descending order
            fireEvent.click(getByText('Value'));
            // Check that the first row has the largest value
            expect(getByText('500')).toBeInTheDocument();
        });
    });
    >>>>>>> REPLACE
    ```
    
    This change adds a new test case that checks if the data is sorted correctly when the 'Value' column header is clicked. It first checks that the data is sorted in ascending order, then in descending order.

--- www/app/src/DataTable.test.js
+++ www/app/src/DataTable.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { useDataLoader } from './DataLoader';
 import DataTable from './DataTable';
@@ -29,5 +29,30 @@ describe('DataTable', () => {
         expect(groceriesElements[0]).toBeInTheDocument();
         expect(groceriesElements[1]).toBeInTheDocument();
         expect(getByText('Test note')).toBeInTheDocument();
+    });
+
+    it('sorts data correctly when column header is clicked', () => {
+        useDataLoader.mockReturnValue({
+            transactions: [
+                { id: 1, date: '2021-01-01', value: 100, payee_name: 'John Doe', category_name: 'Groceries', note: 'Test note' },
+                { id: 2, date: '2021-02-01', value: 200, payee_name: 'Jane Doe', category_name: 'Rent', note: 'Rent payment' },
+                { id: 3, date: '2021-03-01', value: 300, payee_name: 'Bob Smith', category_name: 'Utilities', note: 'Electricity bill' },
+                { id: 4, date: '2021-04-01', value: 400, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
+                { id: 5, date: '2021-05-01', value: 500, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
+            ],
+            error: null,
+        });
+
+        const { getByText } = render(<DataTable searchTerm="" />);
+
+        // Click the 'Value' column header to sort by value in ascending order
+        fireEvent.click(getByText('Value'));
+        // Check that the first row has the smallest value
+        expect(getByText('100')).toBeInTheDocument();
+
+        // Click the 'Value' column header again to sort by value in descending order
+        fireEvent.click(getByText('Value'));
+        // Check that the first row has the largest value
+        expect(getByText('500')).toBeInTheDocument();
     });
 });
