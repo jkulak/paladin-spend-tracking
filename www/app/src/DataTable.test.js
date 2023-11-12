b6d9809ee2a3e244c2232c5be30822f899b6e34d
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-12 17:00:12 +0100

    Changed the note for a transaction in DataTable.test.js.

--- www/app/src/DataTable.test.js
+++ www/app/src/DataTable.test.js
@@ -10,7 +10,7 @@ describe('DataTable', () => {
         { id: 2, date: '2021-02-01', value: 200, payee_name: 'John Doe', category_name: 'Rent', note: 'Rent payment' },
         { id: 3, date: '2021-03-01', value: 300, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Electricity bill' },
         { id: 4, date: '2021-04-01', value: -440, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
-        { id: 5, date: '2021-05-01', value: -550, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
+        { id: 5, date: '2021-05-01', value: -550, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets note' },
     ];
 
     it('loads and displays transactions', () => {
