719fb5a179129f6853b6d715630008e98b5009f1
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-12 20:02:43 +0100

    aider: Refactored `useDataLoader` test to ensure that the hook causes a rerender when called by using `act` and replacing the mock function with a real function that updates a local variable.
    # Aider chat conversation:
    
    USER:  useDataLoader › should fetch data when called
    
        Timed out in waitForNextUpdate after 1000ms.
    
          15 |
          16 |         // Wait for the hook to run useEffect and call the API
        > 17 |         await waitForNextUpdate();
    
    ASSISTANT: The error message "Timed out in waitForNextUpdate after 1000ms" suggests that the `waitForNextUpdate` function from the `react-hooks-testing-library` is timing out. This function waits for the next update of the hook to complete, but if it doesn't complete within the default timeout of 1000ms, it throws an error.
    
    This could be happening because the `useDataLoader` hook isn't causing a rerender, which is what `waitForNextUpdate` is waiting for.
    
    To fix this, we need to ensure that the `useDataLoader` hook causes a rerender when it's called. This can be done by making sure that the `setTransactions` function passed to `useDataLoader` in the test causes a rerender.
    
    Here are the changes:
    
    ```javascript
    www/app/src/DataLoader.test.js
    <<<<<<< SEARCH
    import { renderHook } from '@testing-library/react-hooks';
    import { useDataLoader } from './DataLoader';
    import { mockTransactions } from './mockTransactions';
    
    describe('useDataLoader', () => {
        it('should fetch data when called', async () => {
            // Mock fetch and its response
            global.fetch = jest.fn(() =>
                Promise.resolve({
                    json: () => Promise.resolve(mockTransactions),
                })
            );
    
            const { result, waitForNextUpdate } = renderHook(() => useDataLoader('note', jest.fn(), 'id', 'asc'));
    
            // Wait for the hook to run useEffect and call the API
            await waitForNextUpdate();
    
            // Check if fetch was called
            expect(global.fetch).toHaveBeenCalled();
    
            // Reset fetch to its original implementation
            global.fetch.mockRestore();
        });
    });
    =======
    import { renderHook, act } from '@testing-library/react-hooks';
    import { useDataLoader } from './DataLoader';
    import { mockTransactions } from './mockTransactions';
    
    describe('useDataLoader', () => {
        it('should fetch data when called', async () => {
            // Mock fetch and its response
            global.fetch = jest.fn(() =>
                Promise.resolve({
                    json: () => Promise.resolve(mockTransactions),
                })
            );
    
            let transactions = [];
            const setTransactions = (newTransactions) => {
                transactions = newTransactions;
            };
    
            const { result, waitForNextUpdate } = renderHook(() => useDataLoader('note', setTransactions, 'id', 'asc'));
    
            // Wait for the hook to run useEffect and call the API
            await act(async () => {
                await waitForNextUpdate();
            });
    
            // Check if fetch was called
            expect(global.fetch).toHaveBeenCalled();
    
            // Reset fetch to its original implementation
            global.fetch.mockRestore();
        });
    });
    >>>>>>> REPLACE
    ```
    
    This change wraps the `waitForNextUpdate` call in an `act` function to ensure that all updates related to these actions have been processed and the component tree has been re-rendered before continuing. It also replaces the `jest.fn()` mock function with a real function that updates a local variable, ensuring that the hook causes a rerender when it's called.

--- www/app/src/DataLoader.test.js
+++ www/app/src/DataLoader.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useDataLoader } from './DataLoader';
 import { mockTransactions } from './mockTransactions';
 
@@ -11,10 +11,17 @@ describe('useDataLoader', () => {
             })
         );
 
-        const { result, waitForNextUpdate } = renderHook(() => useDataLoader('note', jest.fn(), 'id', 'asc'));
+        let transactions = [];
+        const setTransactions = (newTransactions) => {
+            transactions = newTransactions;
+        };
+
+        const { result, waitForNextUpdate } = renderHook(() => useDataLoader('note', setTransactions, 'id', 'asc'));
 
         // Wait for the hook to run useEffect and call the API
-        await waitForNextUpdate();
+        await act(async () => {
+            await waitForNextUpdate();
+        });
 
         // Check if fetch was called
         expect(global.fetch).toHaveBeenCalled();
