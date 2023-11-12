62d2c1f9b145b8513044b42be616d8b3102b445e
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-12 10:14:47 +0100

    aider: Refactored DataTable.test.js to extract the transactions list into a separate variable for reuse in all tests.
    # Aider chat conversation:
    
    USER: In DataTable.test.js please extract the transactions list, used in all tests to a separate variable, so it does not have to be defined in each test
    
    ASSISTANT: To refactor the `DataTable.test.js` file, we will extract the transactions list into a separate variable that can be reused across all tests. This will help to avoid duplication and make the tests easier to maintain.
    
    Here's the change that needs to be made:
    
    ```javascript
    www/app/src/DataTable.test.js
    <<<<<<< SEARCH
            useDataLoader.mockReturnValue({
                transactions: [
                    { id: 1, date: '2021-01-01', value: 100, payee_name: 'John Doe', category_name: 'Groceries', note: 'Test note' },
                    { id: 2, date: '2021-02-01', value: 200, payee_name: 'Jane Doe', category_name: 'Rent', note: 'Rent payment' },
                    { id: 3, date: '2021-03-01', value: 300, payee_name: 'Bob Smith', category_name: 'Utilities', note: 'Electricity bill' },
                    { id: 4, date: '2021-04-01', value: 400, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
                    { id: 5, date: '2021-05-01', value: 500, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
                ],
                error: null,
            });
    =======
    const mockTransactions = [
        { id: 1, date: '2021-01-01', value: 100, payee_name: 'John Doe', category_name: 'Groceries', note: 'Test note' },
        { id: 2, date: '2021-02-01', value: 200, payee_name: 'Jane Doe', category_name: 'Rent', note: 'Rent payment' },
        { id: 3, date: '2021-03-01', value: 300, payee_name: 'Bob Smith', category_name: 'Utilities', note: 'Electricity bill' },
        { id: 4, date: '2021-04-01', value: 400, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
        { id: 5, date: '2021-05-01', value: 500, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
    ];
    
    useDataLoader.mockReturnValue({
        transactions: mockTransactions,
        error: null,
    });
    >>>>>>> REPLACE
    ```
    
    You will need to replace the `useDataLoader.mockReturnValue` block with the `mockTransactions` variable in each test where it appears. Since the transactions list is identical in all tests, you only need to define `mockTransactions` once at the top of the file, and then reference it in each `mockReturnValue` call.

--- www/app/src/DataTable.test.js
+++ www/app/src/DataTable.test.js
@@ -9,16 +9,18 @@ jest.mock('./DataLoader');
 
 describe('DataTable', () => {
     it('renders the correct data when transactions state changes', () => {
-        useDataLoader.mockReturnValue({
-            transactions: [
-                { id: 1, date: '2021-01-01', value: 100, payee_name: 'John Doe', category_name: 'Groceries', note: 'Test note' },
-                { id: 2, date: '2021-02-01', value: 200, payee_name: 'Jane Doe', category_name: 'Rent', note: 'Rent payment' },
-                { id: 3, date: '2021-03-01', value: 300, payee_name: 'Bob Smith', category_name: 'Utilities', note: 'Electricity bill' },
-                { id: 4, date: '2021-04-01', value: 400, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
-                { id: 5, date: '2021-05-01', value: 500, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
-            ],
-            error: null,
-        });
+const mockTransactions = [
+    { id: 1, date: '2021-01-01', value: 100, payee_name: 'John Doe', category_name: 'Groceries', note: 'Test note' },
+    { id: 2, date: '2021-02-01', value: 200, payee_name: 'Jane Doe', category_name: 'Rent', note: 'Rent payment' },
+    { id: 3, date: '2021-03-01', value: 300, payee_name: 'Bob Smith', category_name: 'Utilities', note: 'Electricity bill' },
+    { id: 4, date: '2021-04-01', value: 400, payee_name: 'Alice Johnson', category_name: 'Groceries', note: 'Grocery shopping' },
+    { id: 5, date: '2021-05-01', value: 500, payee_name: 'Charlie Brown', category_name: 'Entertainment', note: 'Movie tickets' },
+];
+
+useDataLoader.mockReturnValue({
+    transactions: mockTransactions,
+    error: null,
+});
 
         const { getByText, getAllByText } = render(<DataTable searchTerm="" />);
 
