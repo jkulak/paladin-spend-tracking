02bf453e091d6e35434700c7805e943cea49eb90
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-12 23:00:21 +0100

    aider: Updated test to wait for transactions to be displayed before checking if they are in the document.
    # Aider chat conversation:
    
    USER:  Unable to find an element with the text: /Test note/i. This could be because the text is broken up by multiple elements. In this case, you can provide a function for your text matcher to make your matcher more flexible. The element we are looking for is in the FilteringForm.js file. it is the only input there
    
    ASSISTANT: The issue seems to be that the test is trying to find an element with the text "Test note", but it's not able to find it. This could be because the text is not present in the DOM at the time the test is running.
    
    The `getByLabelText` function is used to find a form control by its label text. In this case, it's looking for an input field with a label that matches the regular expression `/Search/i`. This should match the "Search" label in `FilteringForm.js`.
    
    However, the `getByText` function is used to find an element by its text content. In this case, it's looking for an element with the text "Test note". This text is not present in the `FilteringForm.js` file, so the test fails.
    
    To fix this, we need to modify the test to wait for the transactions to be displayed before trying to find them by their text content. We can use the `findByText` function, which returns a Promise that resolves when an element with the given text is found.
    
    Here is the updated `App.test.js` file:
    
    ```javascript
    www/app/src/App.test.js
    <<<<<<< SEARCH
    import React from 'react';
    import { render, fireEvent, waitFor } from '@testing-library/react';
    import App from './App';
    import { mockTransactions } from './mockTransactions';
    
    // Mock the useDataLoader hook to return the mockTransactions data
    jest.mock('./DataLoader', () => ({
      useDataLoader: jest.fn(() => mockTransactions),
    }));
    
    test('loads and displays transactions', async () => {
      const { getByLabelText, getByText } = render(<App />);
    
      // Simulate entering "note" in the Filtering Form input
      fireEvent.change(getByLabelText(/Search/i), { target: { value: 'note' } });
    
      // Wait for the transactions to be displayed
      await waitFor(() => getByText(/Test note/i));
    
      // Check if the transactions are displayed correctly
      mockTransactions.forEach(transaction => {
        expect(getByText(transaction.note)).toBeInTheDocument();
      });
    });
    =======
    import React from 'react';
    import { render, fireEvent } from '@testing-library/react';
    import App from './App';
    import { mockTransactions } from './mockTransactions';
    
    // Mock the useDataLoader hook to return the mockTransactions data
    jest.mock('./DataLoader', () => ({
      useDataLoader: jest.fn(() => mockTransactions),
    }));
    
    test('loads and displays transactions', async () => {
      const { getByLabelText, findByText } = render(<App />);
    
      // Simulate entering "note" in the Filtering Form input
      fireEvent.change(getByLabelText(/Search/i), { target: { value: 'note' } });
    
      // Check if the transactions are displayed correctly
      for (const transaction of mockTransactions) {
        const element = await findByText(transaction.note);
        expect(element).toBeInTheDocument();
      }
    });
    >>>>>>> REPLACE
    ```
    
    This updated test will wait for each transaction to be displayed before checking if it's in the document. This should fix the issue with the test failing because the transactions are not displayed at the time the test is running.

--- www/app/src/App.test.js
+++ www/app/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 import { mockTransactions } from './mockTransactions';
 
@@ -9,16 +9,14 @@ jest.mock('./DataLoader', () => ({
 }));
 
 test('loads and displays transactions', async () => {
-  const { getByLabelText, getByText } = render(<App />);
+  const { getByLabelText, findByText } = render(<App />);
 
   // Simulate entering "note" in the Filtering Form input
   fireEvent.change(getByLabelText(/Search/i), { target: { value: 'note' } });
 
-  // Wait for the transactions to be displayed
-  await waitFor(() => getByText(/Test note/i));
-
   // Check if the transactions are displayed correctly
-  mockTransactions.forEach(transaction => {
-    expect(getByText(transaction.note)).toBeInTheDocument();
-  });
+  for (const transaction of mockTransactions) {
+    const element = await findByText(transaction.note);
+    expect(element).toBeInTheDocument();
+  }
 });
