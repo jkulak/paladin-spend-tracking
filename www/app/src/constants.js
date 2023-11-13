3a2682282b36e52d3280f4c6403460e0a5ceca07
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-13 18:33:30 +0000

    Update constants.js with new display max and debounce time values

--- www/app/src/constants.js
+++ www/app/src/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = 'http://localhost:3000/transaction_view';
-export const DISPLAY_MAX = 50; // Adjust this value as needed
-export const DEBOUNCE_TIME = 800; // Adjust this value as needed
+export const DISPLAY_MAX = 100; // Adjust this value as needed
+export const DEBOUNCE_TIME = 500; // Adjust this value as needed
