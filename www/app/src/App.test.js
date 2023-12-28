3cfb70cf80d190c68e29e248aba46bc6a0a98e5f
Jakub Kułak <jakub.kulak@gmail.com>
2023-12-29 00:40:45 +0100

    aider: Update loads and displays transactions test to simulate user input, wait for debounce and API request, and check if data is loaded and displayed correctly.
    # Aider chat conversation:
    
    USER: I did `git reset --hard HEAD~1` to discard the last edits.
    
    ASSISTANT: Ok.
    
    USER: In App.test.js Update the loads and displays transactions test so it tests scenario, when user enters "note" in the searchInput input in FilteringForm.js and then debonce waits before the API request, then the request is run and data is loaded to DataTable and displayed. Important - take into account the debouce while testing it. Don't use any extra libraries.
    
    ASSISTANT: To make this change we need to modify `www/app/src/App.test.js`:
    
    1. Import necessary testing libraries and components.
    2. Mock the fetch API to simulate the API request.
    3. Update the test to simulate user input, wait for debounce, and check if the data is loaded and displayed correctly.
    
    Here is the *SEARCH/REPLACE* block:
    
    www/app/src/App.test.js
    ```javascript
    <<<<<<< SEARCH
    import { mockTransactions } from './mockTransactions';
    
    // Mock the useDataLoader hook to return the mockTransactions data
    jest.mock('./DataLoader', () => ({
      useDataLoader: jest.fn(() => mockTransactions),
    }));
    
    test('loads and displays transactions', () => {
    
    });
    =======
    import { render, waitFor, screen } from '@testing-library/react';
    import userEvent from '@testing-library/user-event';
    import { mockTransactions } from './mockTransactions';
    import App from './App';
    import { DEBOUNCE_TIME } from './constants';
    
    // Mock the fetch API
    global.fetch = jest.fn(() =>
      Promise.resolve({
        json: () => Promise.resolve(mockTransactions),
      })
    );
    
    beforeEach(() => {
      fetch.mockClear();
    });
    
    test('loads and displays transactions', async () => {
      render(<App />);
    
      // Simulate user input
      userEvent.type(screen.getByRole('textbox'), 'note');
    
      // Wait for debounce and API request
      await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1), { timeout: DEBOUNCE_TIME + 100 });
    
      // Check if the data is loaded and displayed correctly
      expect(screen.getByText('Test note')).toBeInTheDocument();
    });
    >>>>>>> REPLACE
    ```
    This test will simulate a user entering "note" in the search input, wait for the debounce and the API request, and then check if the data is loaded and displayed correctly. The fetch API is mocked to simulate the API request.

--- www/app/src/App.test.js
+++ www/app/src/App.test.js
@@ -1,10 +1,29 @@
+import { render, waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { mockTransactions } from './mockTransactions';
+import App from './App';
+import { DEBOUNCE_TIME } from './constants';
 
-// Mock the useDataLoader hook to return the mockTransactions data
-jest.mock('./DataLoader', () => ({
-  useDataLoader: jest.fn(() => mockTransactions),
-}));
+// Mock the fetch API
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(mockTransactions),
+  })
+);
 
-test('loads and displays transactions', () => {
+beforeEach(() => {
+  fetch.mockClear();
+});
 
+test('loads and displays transactions', async () => {
+  render(<App />);
+  
+  // Simulate user input
+  userEvent.type(screen.getByRole('textbox'), 'note');
+
+  // Wait for debounce and API request
+  await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1), { timeout: DEBOUNCE_TIME + 100 });
+
+  // Check if the data is loaded and displayed correctly
+  expect(screen.getByText('Test note')).toBeInTheDocument();
 });
